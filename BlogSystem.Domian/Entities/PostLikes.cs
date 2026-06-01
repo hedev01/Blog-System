13ed1638ff2e2d36ed729dbcce15aa84508555ad
Hedeve01 <nazari.fph@gmail.com>
2026-06-01 15:47:10 +0330

    Feat: Add Table PostLikes

---
+++ BlogSystem.Domian/Entities/PostLikes.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogSystem.Domian.Entities
+{
+    public class PostLikes
+    {
+        public int id { get; set; }
+        public bool Like { get; set; }
+        public Guid UserId { get; set; }
+
+        public int PostId
+        {
+            get; set;
+        }
+
+        public DateTime CreateAt
+        {
+            get; set;
+        }
+
+        public PostLikes() { }
+    }
+}
