c747fc267d6472f9c257f11c19b99eba3ea0a9aa
Hedeve01 <nazari.fph@gmail.com>
2026-06-03 03:22:40 +0330

    Refactor: Add Role In claims

--- BlogSystem.Infrastructure/Services/IJwtTokenService.cs
+++ BlogSystem.Infrastructure/Services/IJwtTokenService.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlogSystem.Domian.Enums;
 
 namespace BlogSystem.Infrastructure.Services
 {
     public interface IJwtTokenService
     {
-        string GenerateToken(string userName);
+        string GenerateToken(string userName , Guid userId , Role role);
         string GenerateRefreshToken();
     }
 }
--- BlogSystem.Infrastructure/Services/JwtTokenService.cs
+++ BlogSystem.Infrastructure/Services/JwtTokenService.cs
@@ -6,6 +6,7 @@ using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using BlogSystem.Domian.Enums;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -20,13 +21,13 @@ namespace BlogSystem.Infrastructure.Services
         {
             _config = config;
         }
-        public string GenerateToken(string userName)
+        public string GenerateToken(string userName , Guid userId , Role role)
         {
             var claims = new[]
             {
-                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
                 new Claim(ClaimTypes.Name, userName),
-                //new Claim(ClaimTypes.Role, "Admin") // اگر نقش بندی دارید
+                new Claim(ClaimTypes.Role, role.ToString()) 
             };
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
@@ -35,7 +36,7 @@ namespace BlogSystem.Infrastructure.Services
                 issuer: _config["Jwt:Issuer"],
                 audience: _config["Jwt:Audience"],
                 claims: claims,
-                expires: DateTime.Now.AddMinutes(1),
+                expires: DateTime.Now.AddMinutes(15),
                 signingCredentials: creds
             );
             var generatedToken = new JwtSecurityTokenHandler().WriteToken(token);
